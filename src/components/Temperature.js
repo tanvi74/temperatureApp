eb7fa091f0c960328f7b52e2c4d63ee49dc975f7
maps <1998tanvi@gmail.com>
2020-09-11 16:12:49 +0530

    master

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import "./Temperature.css";
+import "./temperature.css";
 
 class Temperature extends React.Component{
     render(){
         const temp = this.props.temp;
         let hotClass;
-        if(temp>=100){
+        // let style;
+        if(temp >= 100){
+            // style = {
+            //     color: "red",
+            //     backgroundColor: "yellow",
+            //     fontSize: 30
+            // }
             hotClass = 'too-hot'
         }
         return(
             <div className="team-input">
-                <legend className={hotClass}>Enter Temperature in : {this.props.scale}</legend>
+                <legend className={hotClass}>Enter Temperature in : {this.props.scale}</legend> 
+                {/* <legend style={style}>Enter Temperature in : {this.props.scale}</legend> */}
                 <input
                     value = {temp}
                     onChange={(e)=>{this.props.handleChange(e,this.props.scale)}}
